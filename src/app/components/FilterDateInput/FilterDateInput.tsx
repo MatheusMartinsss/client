ebbce86f76b94e82bb4fdb7bd9dcf883d348a2b6
Matheus <matheus.s.martins98@gmail.com>
2023-11-14 15:12:47 -0300

    refactor date input

--- src/app/components/FilterDateInput/FilterDateInput.tsx
+++ src/app/components/FilterDateInput/FilterDateInput.tsx
@@ -1,37 +1,39 @@
-import React, { useState } from 'react';
-import TextField from '@mui/material/TextField';
-import { FormControl} from '@mui/material';
+import React from 'react';
+import TextField, { TextFieldProps } from '@mui/material/TextField';
+import { FormControl } from '@mui/material';
+import { format } from 'date-fns';
 
 interface DateInputProps {
     label?: string;
-    value?: string;
-    onChange: (newValue: string) => void;
+    value?: Date | null;
+    onChange: (newValue: Date | null) => void;
+    props?: TextFieldProps
 }
 
-const FilterDateInput: React.FC<DateInputProps> = ({ label, value, onChange }) => {
+const FilterDateInput: React.FC<DateInputProps> = ({ label, value, onChange, props }) => {
+
+    const valueFormated = value ? format(value, 'yyyy-MM-dd') : ''
 
     const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        onChange(event.target.value);
+        onChange(event.target.value ? new Date(event.target.value) : null);
     };
 
     return (
-        <FormControl fullWidth>
-            <TextField
-                id="date"
-                type="date"
-                label={label}
-                size='small'
-                value={value}
-                onChange={handleDateChange}
-                InputLabelProps={{
-                    shrink: true,
-                }}
-                sx={{
-                    height: 40,
-                }}
+        <TextField
+            {...props}
+            id="date"
+            type="date"
+            label={label}
+            value={valueFormated}
+            onChange={handleDateChange}
+            InputLabelProps={{
+                shrink: true,
+            }}
+            sx={{
+                height: 40,
+            }}
 
-            />
-        </FormControl>
+        />
     );
 };
 
