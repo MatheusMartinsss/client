26330a43bc3a471f34113d3c1d10a21c435265f3
Matheus <matheus.s.martins98@gmail.com>
2024-03-08 23:47:03 -0300

    add decimals case

--- src/utils/unitsFormater.ts
+++ src/utils/unitsFormater.ts
@@ -7,29 +7,23 @@ type converUnitProps = {
 }
 
 export const convertUnit = ({ unit, currentValue, entryValue }: converUnitProps) => {
-    const scale = unit === 'meters' ? 100 : 1000
-    const maxNumberSize = unit === 'meters' ? 5 : 6
-
+    const scale = unit === 'meters' ? 100 : 1000;
+    const maxNumberSize = unit === 'meters' ? 5 : 10;
+    const decimalCases = unit === 'meters' ? 2 : 3;
     const currentValueFormated = String(currentValue).replace(/\D/g, '')
-    const currentValueSize = currentValueFormated.replace(/0/g, '').length
+    const currentValueSize = currentValueFormated.length
 
     const entryValueFormated = entryValue.replace(/\D/g, '')
-    const entryValueSize = entryValueFormated.replace(/0/g, '').length
-
+    const entryValueSize = String(entryValueFormated).length
+    
     if (entryValueSize < currentValueSize) { // Verifies if any number has been erased during input.
-        const newValue = (Number(entryValueFormated) / scale)
-        if (String(newValue).length < maxNumberSize + 1) {
-            return `0${newValue}`
-        }
+        const newValue = (Number(entryValueFormated) / scale).toFixed(decimalCases)
+        return newValue
     }
     if (currentValueSize >= maxNumberSize) { // Verifies if the number does not exceed the maximum number size.
         return currentValue
     }
     let formatedValue = `${currentValueFormated}${entryValue.slice(-1)}`
-    const newValue = (Number(formatedValue) / scale)
-    if (String(newValue).length < maxNumberSize + 1) {
-        return `0${newValue}`
-    }
-    return (Number(formatedValue) / scale)
+    return (Number(formatedValue) / scale).toFixed(decimalCases)
 
 }
